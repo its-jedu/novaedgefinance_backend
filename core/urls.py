06cd42cbacbb14ce73fb003c9fa2bd159e872bce
its_jedu <olajideweb@gmail.com>
2026-03-01 17:38:55 +0100

    Chore: New updated backend code

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import home
-from .management_views import create_superuser_view
+from .management_views import create_default_superuser
 
 
 urlpatterns = [
     path('', home, name='home'),
-    path("create-superuser/", create_superuser_view, name="create_superuser"),
+    path("create-superuser/", create_default_superuser, name="create_superuser"),
 ]
