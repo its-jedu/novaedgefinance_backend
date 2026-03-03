d57cd2e33f96b547a7499c667f0105f22957e33d
its_jedu <olajideweb@gmail.com>
2026-03-03 12:15:49 +0100

    Chore: Implementing migration during deployement

--- core/apps.py
+++ core/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
-
-    def ready(self):
-        from .utils import create_default_superuser_if_needed
-        create_default_superuser_if_needed()
